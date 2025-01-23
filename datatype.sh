59704b4801bed7c5003ea232130382e7c4c1d85d
Charan Suravarapu <charansuravarapu999@gmail.com>
2025-01-23 11:47:20 +0530

    datatype script added again

--- datatype.sh
+++ datatype.sh
@@ -5,7 +5,7 @@ Number2=$2
 
 Timestamp=$(Date)
 
-echo "Script executed at $Timestamp "
+echo "Script executed at $Timestamp"
 Sum=$(($Number1+$Number2))
 
 echo "Sum of $Number1 and $Number2 is: $Sum" 
