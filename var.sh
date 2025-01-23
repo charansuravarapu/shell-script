92bf89d1297a11e9b24f70a7c4b5526ce1c9b48e
Charan Suravarapu <charansuravarapu999@gmail.com>
2025-01-23 10:38:36 +0530

    Updated second script commited again

--- var.sh
+++ var.sh
@@ -1,5 +1,9 @@
-echo "Charan:Hi Murali"
-echo "Murali:Hii Man"
-echo "Charan:what'sapp today"
+Name1="Charan"
+Name2="Murali"
+
+echo "$Name1:Hi $Name2"
+echo "$Name2:Hii Man"
+echo "$Name1:what'supp $Name2"
+
 
 
