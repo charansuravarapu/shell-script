480007627fef25fa1d40acc34174a13c3f87d004
Charan Suravarapu <charansuravarapu999@gmail.com>
2025-01-24 09:33:25 +0530

    mysql install script

--- install.sh
+++ install.sh
@@ -2,17 +2,17 @@
 
 userid=$(id -u)
  
- if [$userid -ne 0]
+ if [ $userid -ne 0 ]
  then 
     echo "Error: you must have sudo access to execute this script"
 fi
 
 dnf list installed mysql
 
-if [$? -ne 0]
+if [ $? -ne 0 ]
 then 
     dnf install mysql -y
-    if [$? -ne 0]
+    if [ $? -ne 0 ]
     then 
         echo "Installing MyQSL...failures"
         exit 1
